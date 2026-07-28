85dbd025ebf604874add5a2a6f8379aa12f341db
yadam1298 <y.n.v.n.kumar@gmail.com>
2026-07-28 17:51:19 +0530

    first commit

--- server/controllers/attendanceController.js
+++ server/controllers/attendanceController.js
@@ -335,7 +335,7 @@ exports.getEmployeeAttendanceByDate = async (req, res) => {
       employee: attendance.employee,
       date: attendance.date,
       punches: attendance.punches,
-      method: attendance.method,
+      method: attendance.punches.method,
       pairs,
       totalMinutes: pairs.reduce((sum, p) => sum + (p.duration || 0), 0),
     });
